cde1d9dfeae5f0d03ac948955bd8c77e07ab60b8
Anghievisck <pedro.luis.angh@gmail.com>
2023-05-17 12:14:02 -0300

    debra layout

---
+++ App/app/src/main/java/com/example/pokejournal/tela_inicial.java
@@ -0,0 +1,14 @@
+package com.example.pokejournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tela_inicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+}
