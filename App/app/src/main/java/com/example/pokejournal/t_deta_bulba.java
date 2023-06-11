afac13536f3c71d72c43e5a55e316d6a90770d7a
Débora Liberato <dliberato014@gmail.com>
2023-06-11 19:00:26 -0300

    Link no card, mais tela de detalhamento sobre o Bulbasaur

---
+++ App/app/src/main/java/com/example/pokejournal/t_deta_bulba.java
@@ -0,0 +1,14 @@
+package com.example.pokejournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class t_deta_bulba extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tdeta_bulba);
+    }
+}
--- App/app/src/main/java/com/example/pokejournal/tela_inicial.java
+++ App/app/src/main/java/com/example/pokejournal/tela_inicial.java
@@ -1,16 +1,29 @@
 package com.example.pokejournal;
-
-import static com.example.pokejournal.PokemonUtil.getPokemon;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class tela_inicial extends AppCompatActivity {
+
+    private ImageView img_bulbB;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+
+    img_bulbB = findViewById(R.id.img_bulba);
+    img_bulbB.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(tela_inicial.this,t_deta_bulba.class);
+            startActivity(intent);
+        }
+    });
+
     }
 }
