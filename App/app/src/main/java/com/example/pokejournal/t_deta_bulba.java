62cfc723afc7731d26cae819f28cc18657238a9d
anghievisck <pedro.luis.angh@gmail.com>
2023-06-14 12:16:48 -0300

    Desenvolvimento do front-end estágios finais

--- App/app/src/main/java/com/example/pokejournal/t_deta_bulba.java
+++ App/app/src/main/java/com/example/pokejournal/t_deta_bulba.java
@@ -2,13 +2,27 @@ package com.example.pokejournal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class t_deta_bulba extends AppCompatActivity {
+
+    private ImageView img_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tdeta_bulba);
+
+        img_menu.findViewById(R.id.img_menu);
+        img_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(t_deta_bulba.this, tela_menu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
