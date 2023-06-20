e57104eb425631af2d862dd5cdc71a27cad37d5b
Débora Liberato <dliberato014@gmail.com>
2023-06-20 16:33:50 -0300

    Incluindo tela de favoritos

--- App/app/src/main/java/com/example/pokejournal/navbar.java
+++ App/app/src/main/java/com/example/pokejournal/navbar.java
@@ -12,10 +12,33 @@ public class navbar extends AppCompatActivity {
     private ImageView botao_inicio;
     private TextView texto_inicio;
 
+    private ImageView menu;
+    private TextView menu2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navbar);
+
+        menu = findViewById(R.id.fundo_fav);
+        menu2= findViewById(R.id.txtfavorito);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (navbar.this, Favoritos.class);
+                startActivity(intent);
+            }
+        });
+
+        menu2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (navbar.this, Favoritos.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     public void goTelaInicial(View v){
@@ -27,4 +50,8 @@ public class navbar extends AppCompatActivity {
         Intent intent = new Intent(navbar.this, QuizActivity.class);
         startActivity(intent);
     }
+
+
+
+
 }
