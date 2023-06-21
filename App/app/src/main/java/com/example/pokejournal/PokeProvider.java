6e33d4f02fbeb2e04a37101a3a3497d5ac36506f
Anghie <pedro.luis.angh@gmail.com>
2023-06-20 22:43:40 -0300

    Implementação do Widget

---
+++ App/app/src/main/java/com/example/pokejournal/PokeProvider.java
@@ -0,0 +1,25 @@
+package com.example.pokejournal;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class PokeProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
+        for(int appWidgetId : appWidgetIds){
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.poke_widget);
+            views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
+
