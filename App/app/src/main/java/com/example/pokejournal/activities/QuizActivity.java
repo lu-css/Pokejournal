5aed47b32db75e3f881e524589a5ee9df62041d3
lu-css <dossantossilvalucas963@gmail.com>
2023-09-17 10:48:36 -0300

    refactor:change async method

--- App/app/src/main/java/com/example/pokejournal/activities/QuizActivity.java
+++ App/app/src/main/java/com/example/pokejournal/activities/QuizActivity.java
@@ -1,13 +1,5 @@
 package com.example.pokejournal.activities;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.AsyncTaskLoader;
-import androidx.loader.content.Loader;
-
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -16,59 +8,34 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.pokejournal.R;
+import com.example.pokejournal.fetchers.pokeapi.FetchRandomPokemon;
 import com.example.pokejournal.helpers.ImageHelper;
 import com.example.pokejournal.models.Pokemon;
-import com.example.pokejournal.requests.PokemonUtil;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
-public class QuizActivity extends AppCompatActivity  implements LoaderManager.LoaderCallbacks<QuizModel> {
-    private static final int SEARCH_POKEMON_LOADER_ID = 0;
-
+public class QuizActivity extends AppCompatActivity  implements FetchRandomPokemon.OnFetchRandomPokemon
+{
     private ImageView img_pokemonQuiz;
     private EditText txt_pokemon;
     private String pokemonName;
-
-    private ImageView img_menu2;
-
     private Bitmap originalBitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         loadViews();
-        startLoaders();
+        FetchRandomPokemon fetchRandomPokemon = new FetchRandomPokemon(this);
+        fetchRandomPokemon.Execute();
     }
 
     private void loadViews(){
         img_pokemonQuiz = findViewById(R.id.pokemon_quiz_img);
         txt_pokemon = findViewById(R.id.resposta_quiz);
     }
-
-    private void startLoaders(){
-        LoaderManager loaderManager = LoaderManager.getInstance(this);
-        loaderManager.initLoader(SEARCH_POKEMON_LOADER_ID, null, this);
-    }
-
-    @NonNull
-    @Override
-    public Loader<QuizModel> onCreateLoader(int id, @Nullable Bundle args) {
-        return new QuizLoader(this, args);
-    }
-
-    @Override
-    public void onLoadFinished(@NonNull Loader<QuizModel> loader, QuizModel data) {
-        if(data.hasError()){
-            Toast.makeText(this, data.errorMessage, Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        renderPokemon(data.pokemon);
-    }
-
-    @Override
-    public void onLoaderReset(@NonNull Loader<QuizModel> loader) { }
 
     private void renderPokemon(Pokemon pokemon){
         this.pokemonName = pokemon.pokeName;
@@ -109,57 +76,13 @@ public class QuizActivity extends AppCompatActivity  implements LoaderManager.Lo
         img_pokemonQuiz.setImageBitmap(originalBitmap);
     }
 
-}
-
-class QuizModel{
-    public Pokemon pokemon;
-    public final String errorMessage;
-    private boolean hasError = false;
-
-    public QuizModel(){
-        this.errorMessage = "";
-        this.hasError = false;
-    }
-
-    public QuizModel(Pokemon pokemon){
-        this.errorMessage = "";
-        this.hasError = false;
-        this.pokemon = pokemon;
-    }
-
-    public QuizModel(String errorMessage){
-        this.errorMessage = errorMessage;
-        this.hasError = true;
-    }
-
-    public boolean hasError(){
-        return hasError;
-    }
-
-}
-class QuizLoader extends AsyncTaskLoader<QuizModel> {
-    public QuizLoader(@NonNull Context context, Bundle args) {
-        super(context);
+    @Override
+    public void onLoadRandomPokemon(Pokemon pokemon) {
+        runOnUiThread(() -> renderPokemon(pokemon) );
     }
 
     @Override
-    protected void onStartLoading(){
-        super.onStartLoading();
-        forceLoad();
-    }
-
-    @Nullable
-    @Override
-    public QuizModel loadInBackground() {
-        try{
-            Pokemon pokemon = PokemonUtil.getRandomPokemon();
-            return new QuizModel(pokemon);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            return new QuizModel("ERRO: " + e.getMessage());
-        }
+    public void onFailRandomPokemon(Exception exception) {
+        Toast.makeText(this, exception.getMessage(), Toast.LENGTH_SHORT).show();
     }
 }
-
-
